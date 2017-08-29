20dd3531e1eb073f1b60b9b16ffc41d2bbd382e8
adnanmc <adnanmcj@gmail.com>
2017-08-29 15:13:34 -0400

    child component display info

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAwq27anlQ57tgPhFvu0mSEkosJPoM3Xl0';
-// create new component that produces some html
-// some html
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar/>
-    </div>
-    );
+YTSearch({key: API_KEY, term: 'surfboards'}, 
+         function(data) {
+           console.log(data);
+         }
+)
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [
+
+        ]};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, 
+        (videos) => {
+          this.setState({ videos: videos});
+        }
+)
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
+    
 }
 
 
